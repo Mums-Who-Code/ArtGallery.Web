6b9ad7ca9925319c57727ccc7729eae7a9e58d6a
NaynaMallick <naynamallick@outlook.com>
2022-04-02 14:42:19 -0700

    INFRA: Initialize Web Project

---
+++ ArtGallery.Web.Api/Program.cs
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+namespace ArtGallery.Web.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+        }
+    }
+}
