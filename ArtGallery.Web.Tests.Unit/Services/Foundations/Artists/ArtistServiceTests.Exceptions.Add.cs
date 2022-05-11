e88eb432395042d8aa7787c5729b7dfd35d88b32
Nayna Mallick <naynamallick@outlook.com>
2022-05-10 19:18:40 -0700

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ ArtGallery.Web.Tests.Unit/Services/Foundations/Artists/ArtistServiceTests.Exceptions.Add.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using ArtGallery.Web.Api.Models.Foundations.Artists;
+using ArtGallery.Web.Api.Models.Foundations.Artists.Exceptions;
+using Moq;
+using Xunit;
+
+namespace ArtGallery.Web.Tests.Unit.Services.Foundations.Artists
+{
+    public partial class ArtistServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyException))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync(
+            Exception criticalDependencyException)
+        {
+            //given
+            Artist someArtist = CreateRandomArtist();
+
+            var failedArtistDependencyException =
+                new FailedArtistDependencyException(criticalDependencyException);
+
+            var expectedArtistDependencyException =
+                new FailedArtistDependencyException(failedArtistDependencyException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostArtistAsync(It.IsAny<Artist>()))
+                    .ThrowsAsync(criticalDependencyException);
+
+            //when
+            ValueTask<Artist> addArtistTask =
+                this.artistService.AddArtistAsync(someArtist);
+
+            //then
+            await Assert.ThrowsAsync<ArtistDependencyException>(() =>
+                addArtistTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedArtistDependencyException))),
+                        Times.Once());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostArtistAsync(It.IsAny<Artist>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.apiBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- ArtGallery.Web.Tests.Unit/Services/Foundations/Artists/ArtistServiceTests.cs
+++ ArtGallery.Web.Tests.Unit/Services/Foundations/Artists/ArtistServiceTests.cs
@@ -2,14 +2,18 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Data.SqlClient;
 using System.Linq.Expressions;
+using System.Runtime.Serialization;
 using ArtGallery.Web.Api.Brokers.Apis;
 using ArtGallery.Web.Api.Brokers.Loggings;
 using ArtGallery.Web.Api.Models.Foundations.Artists;
 using ArtGallery.Web.Api.Models.Services.Foundations.Artists;
 using Moq;
+using RESTFulSense.Exceptions;
 using Tynamix.ObjectFiller;
 using Xeptions;
+using Xunit;
 
 namespace ArtGallery.Web.Tests.Unit.Services.Foundations.Artists
 {
@@ -29,6 +33,36 @@ namespace ArtGallery.Web.Tests.Unit.Services.Foundations.Artists
                 this.loggingBrokerMock.Object);
         }
 
+        public static TheoryData CriticalDependencyException()
+        {
+            string someMessage = GetRandomMesaage();
+            var httpResponseMessage = new HttpResponseMessage();
+            var httpRequestException = new HttpRequestException();
+
+            var httpResponseUrlNoFoundException =
+                new HttpResponseUrlNotFoundException(
+                    httpResponseMessage,
+                    someMessage);
+
+            var unauthorizedHttpResponseException =
+                new HttpResponseUnauthorizedException(
+                    httpResponseMessage,
+                    someMessage);
+
+            return new TheoryData<Exception>
+            {
+                httpRequestException,
+                httpResponseUrlNoFoundException,
+                unauthorizedHttpResponseException
+            };
+        }
+
+        private static string GetRandomMesaage() =>
+            new MnemonicString(wordCount: GetRandomNumer()).GetValue();
+
+        private static int GetRandomNumer() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
         private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedExeption)
         {
             return actualException =>
@@ -39,6 +73,9 @@ namespace ArtGallery.Web.Tests.Unit.Services.Foundations.Artists
 
         private static string GetRandomEmail() =>
           new EmailAddresses().GetValue().ToString();
+
+        private static SqlException GetSqlException() =>
+            (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
 
         public static Artist CreateRandomArtist() =>
             CreateArtistFiller().Create();
