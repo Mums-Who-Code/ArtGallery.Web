50be9c1dbba7675d661ac0ee6ff7799c9c757d69
Nayna Mallick <naynamallick@outlook.com>
2022-05-19 09:12:10 -0700

    ShouldThrowValidationExceptionOnAddIfArtistViewIsInvalidAndLogItAsync -> FAIL

--- ArtGallery.Web.Tests.Unit/Services/Views/ArtistViews/ArtistViewServiceTests.Validations.cs
+++ ArtGallery.Web.Tests.Unit/Services/Views/ArtistViews/ArtistViewServiceTests.Validations.cs
@@ -52,5 +52,78 @@ namespace ArtGallery.Web.Tests.Unit.Services.Views.ArtistViews
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.artistServiceMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfArtistViewIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            //given
+            ArtistView invalidArtistView = new ArtistView
+            {
+                FirstName = invalidText,
+                LastName = invalidText,
+                Email = invalidText,
+                ContactNumber = invalidText,
+                Status = ArtistStatusView.InActive
+            };
+
+            var invalidArtistViewException = new InvalidArtistViewException();
+
+            invalidArtistViewException.AddData(
+               key: nameof(ArtistView.Id),
+               values: "Id is required.");
+
+            invalidArtistViewException.AddData(
+                key: nameof(ArtistView.FirstName),
+                values: "Text is required.");
+
+            invalidArtistViewException.AddData(
+                key: nameof(ArtistView.LastName),
+                values: "Text is required.");
+
+            invalidArtistViewException.AddData(
+                key: nameof(ArtistView.Email),
+                values: "Text is required.");
+
+            invalidArtistViewException.AddData(
+                key: nameof(ArtistView.ContactNumber),
+                values: "Text is required.");
+
+            var expectedArtistViewValidationException =
+                new ArtistViewValidationException(invalidArtistViewException);
+
+            //when
+            ValueTask<ArtistView> addArtistViewTask =
+                this.artistViewService.AddArtistViewAsync(invalidArtistView);
+
+            //then
+            await Assert.ThrowsAsync<ArtistViewValidationException>(() =>
+                addArtistViewTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedArtistViewValidationException))),
+                        Times.Once);
+
+            this.userServiceMock.Verify(service =>
+                service.GetCurrentlyLoggedInUser(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.artistServiceMock.Verify(service =>
+                service.AddArtistAsync(It.IsAny<Artist>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.artistServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
